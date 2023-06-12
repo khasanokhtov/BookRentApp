60e542a3394075da0f92e40b5bf43737f40f01be
khasanokhtov <76727992+khasanokhtov@users.noreply.github.com>
2023-06-12 17:40:05 +0300

    Initial Commit

---
+++ BookRentApp/BookRentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookRentAppApp.swift
+//  BookRentApp
+//
+//  Created by Alex Okhtov on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookRentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
